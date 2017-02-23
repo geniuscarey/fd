e3c841bceee65591e93869d82dce59a39b3a5099
unknown <x00159504@szxbz219.huaweiobz.com>
2017-02-23 11:53:53 +0800

    test

--- gossip/gossiper.go
+++ gossip/gossiper.go
@@ -2,12 +2,36 @@ package gossip
 
 import (
 	"fmt"
+	"math/rand"
 )
 func GetLocalAddress() InetAddress {
 	return InetAddress(0)
 }
 
 type GossipDigest struct {
+    Endpoint InetAddress `json:"endpoint"`
+    Generation int64 `json:"generation"`
+    Version int64 `json:"version"`
+}
+
+func (gd *GossipDigest) GetEndpoint() InetAddress {
+    return gd.Endpoint
+}
+
+func (gd *GossipDigest) GetGeneration() int64{
+    return gd.Generation
+}
+
+func (gd *GossipDigest) GetVersion() int64{
+    return gd.Version
+}
+
+func (gd *GossipDigest) CompareTo(other *GossipDigest) int64{
+    if gd.Generation == other.Generation {
+        return gd.Version - other.Version
+    } else  {
+        return gd.Version - other.Version
+    }
 }
 
 type gossiper struct {
@@ -38,19 +62,88 @@ func (g *gossiper) run() {
 }
 
 func(g *gossiper) CheckStatus() {
+    //TODO, do nothing now
+}
+
+func(g *gossiper) SendGossip(msg string, toEndpoints map[InetAddress]bool) bool {
+    nSend := len(toEndpoints)
+    if nSend < 1 {
+        return false
+    }
+
+    index := 0
+    if nSend > 1 {
+        index = rand.Intn(nSend)
+    }
+
+    var to InetAddress
+    i := 0
+    for k,_ := range(toEndpoints) {
+        if i == index {
+            to = k
+            break
+        }
+        i += 1
+    }
+
+    if _, ok := g.seeds[to]; ok {
+        return true
+    } else {
+        return false
+    }
 }
 
 func(g *gossiper) GossipToLiveMember(msg string) bool {
-	return false
+    if len(g.liveEndpoints) == 0 {
+	    return false
+    }
+
+    return g.SendGossip(msg, g.liveEndpoints)
 }
 
 func(g *gossiper) GossipToUnreachableMember(msg string) {
+    nLive := len(g.liveEndpoints)
+    nUnreachable := len(g.unreachableEndpoints)
+    if nUnreachable > 0 {
+        prob := float64(nUnreachable/(nLive+1))
+        randomFloat := rand.Float64()
+        if randomFloat < prob {
+            g.SendGossip(msg, g.unreachableEndpoints)
+        }
+    }
 }
 
 func(g *gossiper) GossipToSeed(msg string) {
+    nSeeds := len(g.seeds)
+    if nSeeds > 0 {
+        if len(g.liveEndpoints) == 0 {
+            g.SendGossip(msg, g.seeds)
+        }
+
+        prob := float64(nSeeds/(len(g.liveEndpoints)+len(g.unreachableEndpoints)))
+        randomFloat := rand.Float64()
+        if randomFloat < prob{
+            g.SendGossip(msg, g.seeds)
+        }
+    }
 }
 
 func (g *gossiper) makeGossipDigest() []GossipDigest {
-	d := []GossipDigest{}
-	return d
+    var (
+        version int64 = 0
+        generation int64 = 0
+        i int64 = 0
+    )
+
+	gDigests := make([]GossipDigest, len(g.endpointStates))
+    for epAddr,epState := range g.endpointStates {
+        epHeartbeat := epState.GetHeartbeat()
+        version = epHeartbeat.GetVersion()
+        generation= epHeartbeat.GetGeneration()
+        gDigests[i] = GossipDigest{epAddr, generation, version}
+        i += 1
+    }
+
+	return gDigests
 }
+
--- gossip/gossiper_test.go
+++ gossip/gossiper_test.go
@@ -1,14 +1,13 @@
-package main
+package  gossip
 
 import (
-    "fmt"
-    "github.com/geniuscarey/fd/gossip"
+    "testing"
 )
 
-func main() {
+func Test_NewGossipDigest(t *testing.T) {
     var gDigests []GossipDigest
     gDigests = append(gDigests, GossipDigest{0x00000001, 0, 0})
     gDigests = append(gDigests, GossipDigest{0x00000002, 0, 0})
     gDigests = append(gDigests, GossipDigest{0x00000003, 0, 0})
-    fmt.Println(gDigests)
+    t.Logf("%s", gDigests)
 }
